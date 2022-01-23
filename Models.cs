13c31aee32195a263b49c25deabb8efb33c59368
piekarskipete <thexiben@gmail.com>
2022-01-23 22:44:22 +0100

    Added project

---
+++ Models.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WeatherApp
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+    }
+    public class CreateTaskModel
+    {
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+        public string Description { get; set; }
+    }
+    public class UpdateTaskModel
+    {
+        public string Description { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
